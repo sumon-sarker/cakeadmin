216ada9f954a9956feb7a7f750f156b762accf11
sumon-sarker <sumonsarker74@gmail.com>
2016-10-05 02:30:03 +0600

    Updated AppController

--- CakeAdmin/src/Controller/AppController.php
+++ CakeAdmin/src/Controller/AppController.php
@@ -13,7 +13,7 @@ class AppController extends BaseController{
 	public function initialize(){
 		parent::initialize();
 		$this->InitializeAuthentication();
-		$this->GrantPrefixPermission();
+		$this->CheckAuthorization();
 		$this->CakeAdminSettings();
 	}
 
@@ -37,29 +37,70 @@ class AppController extends BaseController{
 		$this->Auth->allow(['login','signup','passwordRecovery']);
 	}
 
-	protected function GrantPrefixPermission(){
+	protected function redirectToAccessDeny(){
+		return $this->redirect([
+				'plugin'=>'CakeAdmin',
+				'prefix'=>false,
+				'controller'=>'users',
+				'action'=>'accessDeny'
+			]
+		);
+	}
+
+	protected function CheckAuthorization(){
 		$user = $this->Auth->user();
+		$this->loadModel('UserGroup');
+
+
+
+
 		if ($user) {
 			$prefix = isset($this->request->params['prefix'])? $this->request->params['prefix'] : false;
-			if ($prefix=='administrator' && $user['prefix_routing']!='administrator') {
-				$this->redirect(
+			switch ($prefix) {
+				case 'administrator':
+						if ($user['prefix_routing']!='administrator') {
+							return $this->redirectToAccessDeny();
+						}
+					break;
+				default:
+						$this->GrantAuthorization();
+					break;
+			}
+			/*Public Variable for VIEW*/
+			$this->CakeAdminUser = $user;
+			$this->set('CakeAdminUser',$user);
+		}
+	}
+
+	protected function GrantAuthorization(){
+		$user = $this->Auth->user();
+		/*For Inactive Users*/
+		if (!$user['active']) {
+			if ($this->request->params['action']!='accessDeny') {
+				$this->Flash->error(__('Your account is currently inactive!'));
+				return $this->redirect(['prefix'=>false,'controller'=>'users','action'=>'accessDeny']);
+			}
+		}
+		$controller = $this->request->params['controller'];
+		$action = $this->request->params['action'];
+
+		if ($this->request->params['action']!='accessDeny') {
+			$this->loadModel('UserGroupPermissions');
+			$authorised = $this->UserGroupPermissions->find('all')->where([
+					'user_group_id'=>$user['user_group_id'],
+					'controller'=>$controller,
+					'action'=>$action
+				])->first();
+			if (!$authorised) {
+				$this->Flash->error(__('You are not authorized to view!'));
+				/*$this->redirect(
 					[
 						'plugin'=>'CakeAdmin',
 						'prefix'=>false,
 						'controller'=>'users',
 						'action'=>'accessDeny'
 					]
-				);
-			}
-			/*Public Variable for VIEW*/
-			$this->CakeAdminUser = $user;
-			$this->set('CakeAdminUser',$user);
-			/*For Inactive Users*/
-			if (!$user['active']) {
-				if ($this->request->params['action']!='accessDeny') {
-					$this->Flash->error(__('Your account is currently inactive!'));
-					return $this->redirect(['prefix'=>false,'controller'=>'users','action'=>'accessDeny']);
-				}
+				);*/
 			}
 		}
 	}
